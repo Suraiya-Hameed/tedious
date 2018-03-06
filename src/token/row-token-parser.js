90da6b74bf5166bcd3e9869829a1f4f7f38f8631
Suraiya Hameed <v-suhame@microsoft.com>
2018-03-06 10:12:10 -0800

    row-token replace recursive cb to loops

--- src/token/row-token-parser.js
+++ src/token/row-token-parser.js
@@ -2,7 +2,7 @@
 
 const valueParse = require('../value-parser');
 
-module.exports = function(parser, colMetadata, options, callback) {
+/*module.exports = function(parser, colMetadata, options, callback) {
   const columns = options.useColumnNames ? {} : [];
 
   const len = colMetadata.length;
@@ -42,4 +42,49 @@ module.exports = function(parser, colMetadata, options, callback) {
       columns: columns
     });
   });
+};
+*/
+
+
+module.exports = function (parser, colMetadata, options, callback) {
+  const columns = options.useColumnNames ? {} : [];
+
+  const len = colMetadata.length;
+
+  let done = () => {
+    callback({
+      name: 'ROW',
+      event: 'row',
+      columns: columns
+    });
+  }
+
+  console.log('- Total columns :', len);
+  for (let i = 0; i < len; i++) {
+    console.log('Parsing column ', i)
+    const columnMetaData = colMetadata[i];
+
+    valueParse(parser, columnMetaData, options, (value) => {
+      const column = {
+        value: value,
+        metadata: columnMetaData
+      };
+
+      if (options.useColumnNames) {
+        if (columns[columnMetaData.colName] == null) {
+          columns[columnMetaData.colName] = column;
+        }
+      } else {
+        columns.push(column);
+      }
+      // console.log('insde value ',value)
+      if (i == len - 1) {
+        return done();
+      }
+    });
+  }
+
+  console.log('- calling Done')
+
+
 };
